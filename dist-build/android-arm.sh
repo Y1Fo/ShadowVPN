987980a86a46de9fc4fd212170d6c8410c12cd8b
pexcn <pexcn97@gmail.com>
2016-08-04 23:14:15 +0800

    Use -O3 flag for NDK compiler

--- dist-build/android-arm.sh
+++ dist-build/android-arm.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export CFLAGS="-Os -mthumb -marm -march=armv6"
+export CFLAGS="-O3 -mthumb -marm -march=armv6"
 TARCH=arm TARGET_ARCH=arm HOST_COMPILER=arm-linux-androideabi "$(dirname "$0")/android-build.sh"
--- dist-build/android-mips.sh
+++ dist-build/android-mips.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export CFLAGS="-Os"
+export CFLAGS="-O3"
 TARCH=mips TARGET_ARCH=mips HOST_COMPILER=mipsel-linux-android "$(dirname "$0")/android-build.sh"
